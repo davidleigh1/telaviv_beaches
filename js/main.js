3f213c39ad6f74d48f01c3dad4c933ac6495b0bc
davidleigh1 <davidleigh1@gmail.com>
2017-04-13 11:15:37 +0300

    Upload to Git

---
+++ js/main.js
@@ -0,0 +1,723 @@
+tlv_data = [
+{
+	"he": {
+		"name": "<b>צוק</b> צפון - הגבול עם הרצליה",
+		"notes/access": "חוף אסור לרחצה",
+	},
+	"name": "<b>HaTzuk</b> North - Border with Hertzilya",
+	"lat": "32.14511531",
+	"lon": "34.79235336",
+	"aka": "",
+	"type_order": "",
+	"overall_order": "",
+	"cross_street": "",
+	"close_streets": "",
+	"approx_address": "",
+	"restaurants": "-",
+	"hotels": "-",
+	"notes/access": "North of the official HaTzuk North beach up to border with Hertzilya. Swimming is forbidden.",
+	"landmarks": "",
+	"has_lifeguard": false,
+	"is_official": false,
+	"has_gates": false,
+	"vehicle_access": false,
+	"type": "beach"
+},{
+	"he":{
+		"name": "<b>הצוק</b> צפון - מנדרין",
+		"hotels": "מלון מנדרין",
+		"close_streets": "יוניצ'מן ",
+	},
+	"name": "<b>HaTzuk</b> North / Mandarin",
+	"lat": "32.14366638",
+	"lon": "34.79220048",
+	"aka": "",
+	"type_order": "",
+	"overall_order": "",
+	"cross_street": "2170 Yunitzman",
+	"close_streets": "Yunitsman Juniczman",
+	"approx_address": "",
+	"restaurants": "Mandarin Espresso Bar, Hazuk Beach Restaurant",
+	"hotels": "Mandarin Colony",
+	"notes/access": "",
+	"landmarks": "",
+	"has_lifeguard": true,
+	"is_official": true,
+	"has_gates": false,
+	"vehicle_access": false,
+	"type": "beach"
+},{
+	"he": {
+		"name": "<b>הצוק</b> דרום - סי אנד סאן",
+		"aka": "חוף הצוק הדרומי",
+		"cross_street": "סאמט שמעון",
+		"close_streets": "רוזנבלום הרצל ד\"ר",
+	},
+	"name": "<b>HaTzuk</b> South / Sea and Sun",
+	"lat": "32.14007574",
+	"lon": "34.79108065",
+	"aka": "",
+	"type_order": "",
+	"overall_order": "",
+	"cross_street": "Shimon Samat Samet",
+	"close_streets": "2170 2040 Rozenblum Hertzl",
+	"approx_address": "Shimon Samet",
+	"restaurants": "Banana Beach Cliff Beach",
+	"hotels": "West Hotel",
+	"notes/access": "Grid ref is dead end. Is there a way down?",
+	"landmarks": "",
+	"has_lifeguard": true,
+	"is_official": true,
+	"has_gates": true,
+	"vehicle_access": false,
+	"type": "beach"
+},{
+	"he": {
+		"name": "<b>תל ברוך</b> צפון"
+	},
+	"name": "<b>Tel Baruch</b> North to HaTzuk",
+	"lat": "32.13101336",
+	"lon": "34.78767693",
+	"aka": "Sand dunes between Tel Baruch and HaTzuk South / Sea and Sun",
+	"type_order": "",
+	"overall_order": "",
+	"cross_street": "",
+	"close_streets": "",
+	"approx_address": "",
+	"restaurants": "",
+	"hotels": "",
+	"notes/access": "Access is by foot decending from the entrance to the military post behind (and overlooking) the Paz Gas Station on Road 2040.",
+	"landmarks": "",
+	"has_lifeguard": false,
+	"is_official": false,
+	"has_gates": false,
+	"vehicle_access": false,
+	"type": "beach"
+},{
+	"he": {
+		"name": "<b>תל ברוך</b>",
+		"restaurants": "כחול"
+	},
+	"name": "<b>Tel Baruch</b>",
+	"lat": "32.12120613",
+	"lon": "34.78268603",
+	"aka": "",
+	"type_order": "",
+	"overall_order": "",
+	"cross_street": "",
+	"close_streets": "",
+	"approx_address": "Itzhak Artzi St 1, Tel Aviv-Yafo, 6963501, Israel",
+	"restaurants": "Blue",
+	"hotels": "-",
+	"notes/access": "",
+	"landmarks": "",
+	"has_lifeguard": true,
+	"is_official": true,
+	"has_gates": false,
+	"vehicle_access": true,
+	"type": "beach"
+},{
+	"he": {
+		"name": "<b>תל ברוך</b> כלבים"
+	},
+	"name": "Tel Baruch Dog (South)",
+	"lat": "",
+	"lon": "",
+	"type": "beach"
+},{
+	"he": {
+		"name": ""
+	},
+	"name": "<b>Sde Dov</b> Promenade<br>(from North via Tel Baruch)",
+	"lat": "32.11924682",
+	"lon": "34.78222668",
+	"type": "beach"
+},{
+	"he": {
+		"name": ""
+	},
+	"name": "<b>Sde Dov</b> Promenade<br>(from South via Namal Tel Aviv)",
+	"lat": "32.10404348",
+	"lon": "34.77670133",
+	"type": "beach"
+},{
+	"he": {
+		"name": ""
+	},
+	"name": "<b>Reading</b> Power Station",
+	"lat": "32.10288472",
+	"lon": "34.77779835",
+	"aka": "Tachanat Koach, תחנת הכוח רדינג, Pedestrian Bridge",
+	"type": "landmark",
+	"type_order": "",
+	"overall_order": "",
+	"cross_street": "",
+	"close_streets": "",
+	"approx_address": "",
+	"restaurants": "",
+	"hotels": "",
+	"notes/access": "",
+	"landmarks": "",
+	"has_lifeguard": "",
+	"is_official": "",
+	"has_gates": "",
+	"vehicle_access": ""
+},{
+	"he": {
+		"name": "נמל תל אביב"
+	},
+	"name": "Namal Tel Aviv",
+	"lat": "",
+	"lon": "",
+	"aka": "Tel Aviv Harbor Harbour",
+	"type_order": "",
+	"overall_order": "",
+	"cross_street": "",
+	"close_streets": "",
+	"approx_address": "",
+	"restaurants": "",
+	"hotels": "",
+	"notes/access": "",
+	"landmarks": "",
+	"has_lifeguard": "",
+	"is_official": "",
+	"has_gates": "",
+	"vehicle_access": "",
+	"type": "landmark",
+},{
+	"he": {
+		"name": "מציצים"
+	},
+	"name": "Metzitzim",
+	"lat": "32.09387091",
+	"lon": "34.77124169",
+	"aka": "",
+	"type_order": "",
+	"overall_order": "",
+	"cross_street": "",
+	"close_streets": "",
+	"approx_address": "",
+	"restaurants": "-",
+	"hotels": "-",
+	"notes/access": "",
+	"landmarks": "",
+	"has_lifeguard": true,
+	"is_official": true,
+	"has_gates": false,
+	"vehicle_access": true,
+	"type": "beach"
+},{
+	"he": {
+		"name": "הנפרד/הדתיים (נורדאו)"
+	},
+	"name": "Religious / Nordau",
+	"lat": "32.09309378",
+	"lon": "34.77127656",
+	"aka": "Separated Separate Segregated Religious Beach",
+	"type_order": "",
+	"overall_order": "",
+	"cross_street": "",
+	"close_streets": "",
+	"approx_address": "",
+	"restaurants": "-",
+	"hotels": "-",
+	"notes/access": "Vehicle access via Metzitzim (from North)",
+	"landmarks": "",
+	"has_lifeguard": true,
+	"is_official": true,
+	"has_gates": true,
+	"vehicle_access": true,
+	"type": "beach"
+},{
+	"he": {
+		"name": ""
+	},
+	"name": "Park HaAtzmaut (North)",
+	"lat": "",
+	"lon": "",
+	"aka": "Independence Park North",
+	"type_order": "",
+	"overall_order": "",
+	"cross_street": "Nordau",
+	"close_streets": "Ha-Yarkon",
+	"approx_address": "",
+	"restaurants": "",
+	"hotels": "",
+	"notes/access": "",
+	"landmarks": "",
+	"has_lifeguard": "",
+	"is_official": "",
+	"has_gates": "",
+	"vehicle_access": "",
+	"type": "park",
+},{
+	"he": {
+		"name": "הילטון - הכלבים"
+	},
+	"name": "Hilton - Dogs",
+	"lat": "",
+	"lon": "",
+	"aka": "HaKlavim",
+	"type_order": "",
+	"overall_order": "",
+	"cross_street": "",
+	"close_streets": "",
+	"approx_address": "",
+	"restaurants": "",
+	"hotels": "",
+	"notes/access": "",
+	"landmarks": "",
+	"has_lifeguard": false,
+	"is_official": false,
+	"has_gates": false,
+	"vehicle_access": "??",
+	"type": "beach"
+},{
+	"he": {
+		"name": "הילטון - גיי / העצמאות"
+	},
+	"name": "Hilton - Gay / HaAtzmaut",
+	"lat": "",
+	"lon": "",
+	"aka": "Atzmaut Independence",
+	"type_order": "",
+	"overall_order": "",
+	"cross_street": "",
+	"close_streets": "",
+	"approx_address": "",
+	"restaurants": "",
+	"hotels": "",
+	"notes/access": "",
+	"landmarks": "",
+	"has_lifeguard": "",
+	"is_official": "",
+	"has_gates": "",
+	"vehicle_access": "",
+	"type": "beach"
+},{
+	"he": {
+		"name": "הילטון - הגולשים"
+	},
+	"name": "Hilton - Surfers",
+	"lat": "",
+	"lon": "",
+	"aka": "HaGolshim",
+	"type_order": "",
+	"overall_order": "",
+	"cross_street": "",
+	"close_streets": "",
+	"approx_address": "",
+	"restaurants": "",
+	"hotels": "",
+	"notes/access": "",
+	"landmarks": "",
+	"has_lifeguard": "",
+	"is_official": "",
+	"has_gates": "",
+	"vehicle_access": "",
+	"type": "beach"
+},{
+	"he": {
+		"name": "גן העצמאות (דרום)"
+	},
+	"name": "Park HaAtzmaut (South)",
+	"lat": "",
+	"lon": "",
+	"aka": "Independence Park South",
+	"type": "park",
+	"type_order": "",
+	"overall_order": "",
+	"cross_street": "Alozorov",
+	"close_streets": "Eliezer Peri",
+	"approx_address": "",
+	"restaurants": "",
+	"hotels": "",
+	"notes/access": "",
+	"landmarks": "",
+	"has_lifeguard": "",
+	"is_official": "",
+	"has_gates": "",
+	"vehicle_access": ""
+},{
+	"he": {
+		"name": "גורדון"
+	},
+	"name": "Gordon",
+	"lat": "",
+	"lon": "",
+	"type": "beach"
+},{
+	"he": {
+		"name": "פרישמן"
+	},
+	"name": "Frishman",
+	"lat": "",
+	"lon": "",
+	"type": "beach"
+},{
+	"he": {
+		"name": "בוגרשוב"
+	},
+	"name": "Bograshov",
+	"lat": "",
+	"lon": "",
+	"type": "beach"
+},{
+	"he": {
+		"name": "טרומפלדור"
+	},
+	"name": "Trumpeldor",
+	"lat": "",
+	"lon": "",
+	"type": "beach"
+},{
+	"he": {
+		"name": "ירושלים (אלנבי)"
+	},
+	"name": "Jerusalem",
+	"lat": "",
+	"lon": "",
+	"cross_street": "Alenby Allenby",
+	"close_streets": "",	
+	"type": "beach"
+},{
+	"he": {
+		"name": "גאולה"
+	},
+	"name": "Geula",
+	"lat": "",
+	"lon": "",
+	"cross_street": "Geula גאולה",
+	"type": "beach"
+},{
+	"he": {
+		"name": "אביב / Banana"
+	},
+	"name": "Banana / Aviv",
+	"lat": "",
+	"lon": "",
+	"type": "beach"
+},{
+	"he": {
+		"name": "חוף המתופפים"
+	},
+	"name": "Drum / Tupim",
+	"lat": "",
+	"lon": "",
+	"type": "beach"
+},{
+	"he": {
+		"name": "צ'ארלס קלור (צפון)"
+	},
+	"name": "Charles Clore Park (North)",
+	"lat": "",
+	"lon": "",
+	"aka": "Gan Charles Klore South, Claude Clor",
+	"type": "park",
+	"type_order": "",
+	"overall_order": "",
+	"cross_street": "",
+	"close_streets": "Kaufmann",
+	"approx_address": "גן צ'רלס קלור, נחום גולמן 2, ת\"א",
+	"restaurants": "",
+	"hotels": "David Intercontinental, Dan Panorama",
+	"notes/access": "",
+	"landmarks": "",
+	"has_lifeguard": "",
+	"is_official": "",
+	"has_gates": "",
+	"vehicle_access": ""
+},{
+	"he": {
+		"name": ""
+	},
+	"name": "Manta Ray",
+	"lat": "",
+	"lon": "",
+	"aka": "Gan Charles Klore South, Claude Clor",
+	"type": "restaurant",
+	"type_order": "",
+	"overall_order": "",
+	"cross_street": "HaMered",
+	"close_streets": "Kaufmann",
+	"approx_address": "",
+	"restaurants": "Manta Ray",
+	"hotels": "",
+	"notes/access": "",
+	"landmarks": "",
+	"has_lifeguard": "",
+	"is_official": "",
+	"has_gates": "",
+	"vehicle_access": ""
+},{
+	"he": {
+		"name": ""
+	},
+	"name": "Alma",
+	"lat": "",
+	"lon": "",
+	"type": "beach"
+},{
+	"he": {
+		"name": ""
+	},
+	"name": "Alma Dog",
+	"lat": "",
+	"lon": "",
+	"type": "dog-beach"
+},{
+	"he": {
+		"name": ""
+	},
+	"name": "Charles Clore Park (South)",
+	"lat": "",
+	"lon": "",
+	"aka": "Gan Charles Klore South, Claude Clor",
+	"type": "park",
+	"type_order": "",
+	"overall_order": "",
+	"cross_street": "",
+	"close_streets": "Nahum Goldman",
+	"approx_address": "גן צ'רלס קלור, נחום גולמן 2, ת\"א",
+	"restaurants": "",
+	"hotels": "",
+	"notes/access": "",
+	"landmarks": "",
+	"has_lifeguard": "",
+	"is_official": "",
+	"has_gates": "",
+	"vehicle_access": ""
+},{
+	"he": {
+		"name": ""
+	},
+	"name": "Homat HaYam Promenade",
+	"lat": "",
+	"lon": "",
+	"type": "beach"
+},{
+	"he": {
+		"name": ""
+	},
+	"name": "Retsif ha-Aliya ha-Shniya",
+	"lat": "",
+	"lon": "",
+	"type": "beach"
+},{
+	"he": {
+		"name": ""
+	},
+	"name": "Midron Park",
+	"lat": "",
+	"lon": "",
+	"aka": "Namal Yafo Park",
+	"type_order": "",
+	"overall_order": "",
+	"cross_street": "",
+	"close_streets": "",
+	"approx_address": "",
+	"restaurants": "",
+	"hotels": "",
+	"notes/access": "",
+	"landmarks": "",
+	"has_lifeguard": "",
+	"is_official": "",
+	"has_gates": "",
+	"vehicle_access": "",
+	"type": "park"
+},{
+	"he": {
+		"name": ""
+	},
+	"name": "Yafo / Gan Hamidron",
+	"lat": "",
+	"lon": "",
+	"aka": "",
+	"type_order": "",
+	"overall_order": "",
+	"cross_street": "",
+	"close_streets": "Harry Truman",
+	"approx_address": "",
+	"restaurants": "Cassis",
+	"hotels": "",
+	"notes/access": "Access via cycle paths through Midron Park",
+	"landmarks": "",
+	"has_lifeguard": "",
+	"is_official": false,
+	"has_gates": false,
+	"vehicle_access": false,
+	"type": "beach"
+},{
+	"he": {
+		"name": "גבעת עלייה"
+	},
+	"name": "Givat Aliya",
+	"lat": "",
+	"lon": "",
+	"aka": "",
+	"type_order": "",
+	"overall_order": "",
+	"cross_street": "Shtei HaAkhayot (N) Kidron (S)" ,
+	"close_streets": "Bat Galim (N) Yam Suf",
+	"approx_address": "Bat Galim St 8, Tel Aviv-Yafo, Israel",
+	"restaurants": "Babai (N) Cassis (S)",
+	"hotels": "Beit Sidi",
+	"notes/access": "",
+	"landmarks": "",
+	"has_lifeguard": "",
+	"is_official": true,
+	"has_gates": false,
+	"vehicle_access": true,
+	"type": "beach"
+}
+];
+
+
+
+
+// חוף גבעת עליה
+
+
+
+// חוף ירושלים
+// חוף מציצים
+// חוף נורדאו נפרד
+// חוף פרישמן
+// חוף צ'רלס קלור
+
+
+navigateTo = function (lat, lon, getIcon) {
+	console.log("navigateTo",lat, lon);
+    // Mobile devices - open Waze
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+		if (getIcon){
+			return "waze2.png";
+		}
+        var wazeURL = "waze://?ll=" + lat + "," + lon + "&navigate=yes";
+        if (navigator.userAgent.match(/Android/i)) {
+            document.location = wazeURL;
+        } else {
+            window.location.replace(wazeURL);
+        }
+    }
+    // PC - open Google maps
+    else {
+		if (getIcon){
+			return "google-maps.png";
+		}
+        window.open("http://www.google.com/maps/place/" + lat + "," + lon, '_blank');
+    }
+};
+
+createTableFromData = function (data) {
+	var tableHtml = '';
+	var currentRowHtml, en_name, he_name;
+	var placeIcon = "place.png";
+	for (var i = 0, length = data.length; i < length; i++) {
+		// currentRowHtml = '<tr><td>' + data[i].join('</td><td>') + '</td></tr>';
+		// tableHtml += currentRowHtml;
+
+		switch( data[i].type ) {
+		case 'beach':
+			placeIcon = "beach.png"
+			break;
+		case 'park':
+			placeIcon = "park.png"
+			break;
+		case 'restaurant':
+			placeIcon = "spoonfork.png"
+			break;
+		case 'landmark':
+			placeIcon = "place.png"
+			break;
+		default:
+			placeIcon = "place.png"
+		};
+
+
+		if (data[i].lat && data[i].lon) {
+			map_icon = navigateTo(false,false,true) || "route2.png";
+			navigateToSpan = '<img class="navigateTo route-icon pull-right" onclick="navigateTo(\''+ data[i].lat  +'\', \''+ data[i].lon  +'\')" data-lat="'+ data[i].lat  +'" data-lon="'+ data[i].lon  +'" src="img/icons/' + map_icon + '">';
+		} else {
+			navigateToSpan = '';
+		}
+
+		en_name = data[i].name;
+		he_name = data[i]["he"] && data[i]["he"].name || en_name;
+
+		currentRowHtml = 	'<div class="col-md-12 place-line '+ data[i].type  +'">' +
+							'<div class="place-name en"><img class="place-icon" src="img/icons/'+ placeIcon +'">' + en_name + '</div>' +
+							'<div class="place-name he"><img class="place-icon" src="img/icons/'+ placeIcon +'">' + he_name + '</div>' +
+							navigateToSpan +
+							'<div class="place-detail">' + data[i].name + ' is a beach in Tel Aviv</div>' +
+							'</div>';
+		tableHtml += currentRowHtml;
+
+	}
+	return tableHtml;
+}
+
+updateLang = function (specifyLang) {
+	if (!specifyLang){	
+		/* Set specific language if provided */
+		$('html').attr('lang', function(index, attr){
+			return attr == 'en' ? 'he' : 'en';
+		});
+		$('html').attr('dir', function(index, attr){
+			return attr == 'ltr' ? 'rtl' : 'ltr';
+		});
+	} else {
+		/* If not, just toggle between 'he' and 'en' */
+		$('html').attr('lang',specifyLang);
+		$('html').attr('dir', (specifyLang != 'he')?'ltr':'rtl' );
+	}
+	var currentLang = $('html').attr('lang');
+	var previousLang = currentLang == 'en' ? 'he' : 'en';
+	/* Not swapping content.  All content is added to DOM.  Simply showing and hiding based the selected lang */
+	$('.'+currentLang).show();
+	$('.'+previousLang).hide();
+
+	/* Reverse pull left and pull right */
+	if ( $('html').attr('dir') == 'rtl') {
+		$('.pull-right').each(function() {
+			if($(this).parent().css('direction') == 'rtl')
+			$(this).attr('style', 'float:left !important');
+		});
+	} else {
+		$('.pull-right').each(function() {
+			if($(this).parent().css('direction') == 'ltr')
+			$(this).attr('style', 'float:right !important');
+		});		
+	}
+
+
+};
+
+$( document ).ready(function() {
+	console.log( "ready!" );
+
+	/* DOM GENERATION */
+		var textToAppend = createTableFromData(tlv_data);
+		$('#myContainer').replaceWith(textToAppend);
+
+	/* Simulate effect of click to hide the non-selected language */
+		if ( !$('html').attr('lang') ){
+			updateLang("he");
+		}
+
+	/* EVENT HANDLERS */
+
+		/* Change Language */
+		$( ".toggleLang" ).click(function() {
+			updateLang();
+		});
+
+		/* Close Menu */
+		$( ".closeOnClick" ).click(function() {
+			$('.navbar-toggle').click();
+		});
+
+
+
+
+});
